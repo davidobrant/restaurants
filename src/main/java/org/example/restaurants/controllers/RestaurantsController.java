e3cf6b926753664e219e279d98651881bce35595
David Obrant <david.obrant@gmail.com>
2024-06-02 18:46:18 +0200

    fixes

--- src/main/java/org/example/restaurants/controllers/RestaurantsController.java
+++ src/main/java/org/example/restaurants/controllers/RestaurantsController.java
@@ -1,26 +1,20 @@
 package org.example.restaurants.controllers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.restaurants.entities.OpeningHour;
 import org.example.restaurants.entities.Restaurant;
 import org.example.restaurants.exceptions.NotFoundException;
 import org.example.restaurants.services.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
-import java.util.UUID;
 
 @Controller
 @RequestMapping("/restaurants")
 public class RestaurantsController {
-
 
     @Autowired
     RestaurantService restaurantService;
