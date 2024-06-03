26b9f46760da79ffc068e5db057a7ea2ec4903eb
David Obrant <david.obrant@gmail.com>
2024-06-04 00:26:29 +0200

    comment tests

--- src/test/java/org/example/restaurants/RestaurantServiceTests.java
+++ src/test/java/org/example/restaurants/RestaurantServiceTests.java
@@ -18,33 +18,33 @@ import static org.mockito.Mockito.*;
 
 public class RestaurantServiceTests {
 
-    @Mock
-    private RestaurantRepository restaurantRepository;
-
-    @InjectMocks
-    private RestaurantService restaurantService;
-
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
-
-    @Test
-    void TestGetRestaurantByIdIfValid() {
-        long id = 1L;
-        Restaurant restaurant = new Restaurant();
-        restaurant.setId(id);
-        when(restaurantRepository.findById(id)).thenReturn(Optional.of(restaurant));
-        try {
-            Restaurant found = restaurantService.getRestaurantById(id);
-            assertNotNull(found);
-            assertEquals(id, found.getId());
-            System.out.println("Found restaurant successfully");
-        } catch (Exception e) {
-            e.printStackTrace();
-            fail("Failed with exception " + e.getMessage());
-        }
-    }
+//    @Mock
+//    private RestaurantRepository restaurantRepository;
+//
+//    @InjectMocks
+//    private RestaurantService restaurantService;
+//
+//    @BeforeEach
+//    void setUp() {
+//        MockitoAnnotations.openMocks(this);
+//    }
+//
+//    @Test
+//    void TestGetRestaurantByIdIfValid() {
+//        long id = 1L;
+//        Restaurant restaurant = new Restaurant();
+//        restaurant.setId(id);
+//        when(restaurantRepository.findById(id)).thenReturn(Optional.of(restaurant));
+//        try {
+//            Restaurant found = restaurantService.getRestaurantById(id);
+//            assertNotNull(found);
+//            assertEquals(id, found.getId());
+//            System.out.println("Found restaurant successfully");
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            fail("Failed with exception " + e.getMessage());
+//        }
+//    }
 
 //    @Test
 //    void TestGetRestaurantByIdIfInvalid() {
@@ -62,41 +62,41 @@ public class RestaurantServiceTests {
 //        }
 //    }
 
-    @Test
-    void testAddRestaurant() {
-        Restaurant restaurant = new Restaurant();
-        double rating = 3;
-        restaurant.setName("O'Learys");
-        restaurant.setDescription("Sportsbar");
-        restaurant.setCity("Uppsala");
-//        restaurant.setOpen(true);
-        restaurant.setRating(rating);
-        restaurant.setPhone("0705345234");
-        restaurant.setStreet("Dragarbrunnsgata 45");
-        restaurant.setZip("72460");
-        restaurant.setEmail("donotemailus@youwillbeignored.com");
-
-        when(restaurantRepository.save(any(Restaurant.class))).thenReturn(restaurant);
-
-        try {
-            Restaurant created = restaurantService.saveRestaurant(restaurant);
-            assertNotNull(created);
-            assertEquals("O'Learys", created.getName());
-            assertEquals("Sportsbar", created.getDescription());
-            assertEquals("Uppsala", created.getCity());
-//            assertEquals(true, created.isOpen());
-            assertEquals(rating, created.getRating());
-            assertEquals("0705345234", created.getPhone());
-            assertEquals("Dragarbrunnsgata 45", created.getStreet());
-            assertEquals("72460", created.getZip());
-            assertEquals("donotemailus@youwillbeignored.com", created.getEmail());
-            System.out.println("Restaurant added successfully");
-        } catch (Exception e) {
-            e.printStackTrace();
-            fail("Failed with exception " + e.getMessage());
-        }
-
-    }
+//    @Test
+//    void testAddRestaurant() {
+//        Restaurant restaurant = new Restaurant();
+//        double rating = 3;
+//        restaurant.setName("O'Learys");
+//        restaurant.setDescription("Sportsbar");
+//        restaurant.setCity("Uppsala");
+////        restaurant.setOpen(true);
+//        restaurant.setRating(rating);
+//        restaurant.setPhone("0705345234");
+//        restaurant.setStreet("Dragarbrunnsgata 45");
+//        restaurant.setZip("72460");
+//        restaurant.setEmail("donotemailus@youwillbeignored.com");
+//
+//        when(restaurantRepository.save(any(Restaurant.class))).thenReturn(restaurant);
+//
+//        try {
+//            Restaurant created = restaurantService.saveRestaurant(restaurant);
+//            assertNotNull(created);
+//            assertEquals("O'Learys", created.getName());
+//            assertEquals("Sportsbar", created.getDescription());
+//            assertEquals("Uppsala", created.getCity());
+////            assertEquals(true, created.isOpen());
+//            assertEquals(rating, created.getRating());
+//            assertEquals("0705345234", created.getPhone());
+//            assertEquals("Dragarbrunnsgata 45", created.getStreet());
+//            assertEquals("72460", created.getZip());
+//            assertEquals("donotemailus@youwillbeignored.com", created.getEmail());
+//            System.out.println("Restaurant added successfully");
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            fail("Failed with exception " + e.getMessage());
+//        }
+//
+//    }
 
 //    @Test
 //    void updateRestaurantTest() {
@@ -122,69 +122,69 @@ public class RestaurantServiceTests {
 //        }
 //    }
 
-    @Test
-    void testDeleteRestaurant() {
-        long id = 1L;
-        doNothing().when(restaurantRepository).deleteById(id);
-
-        try {
-            restaurantService.deleteRestaurantById(id);
-            verify(restaurantRepository, times(1)).deleteById(id);
-            System.out.println("Deleted succesfully");
-        } catch (Exception e) {
-            e.printStackTrace();
-            fail("Deleting failed with exception " + e.getMessage());
-        }
-    }
-
-    @Test
-    void testDeleteAllRestaurants(){
-        Restaurant restaurant1 = new Restaurant();
-        restaurant1.setId(1L);
-        restaurant1.setName("Restaurant 1");
-
-        Restaurant restaurant2 = new Restaurant();
-        restaurant2.setId(2L);
-        restaurant2.setName("Restaurant 2");
-        System.out.println("Restaurants Ids are : " + restaurant1 + restaurant2);
-        doNothing().when(restaurantRepository).deleteAll();
-        try {
-            restaurantService.deleteAllRestaurants();
-            verify(restaurantRepository, times(1)).deleteAll();
-            System.out.println("Deleted succesfully");
-        } catch (Exception e) {
-            e.printStackTrace();
-            fail("Deleting failed with exception " + e.getMessage());
-        }
-    }
-    @Test
-    void testGetAllCities() {
-        Restaurant restaurant1 = new Restaurant();
-        restaurant1.setCity("Gothenburg");
-
-        Restaurant restaurant2 = new Restaurant();
-        restaurant2.setCity("Stockholm");
-
-        Restaurant restaurant3 = new Restaurant();
-        restaurant3.setCity("Uppsala");
-
-        List<Restaurant> mockRestaurants = List.of(restaurant1, restaurant2, restaurant3);
-        when(restaurantRepository.findAll()).thenReturn(mockRestaurants);
-
-        System.out.println("Cities are: " + mockRestaurants);
-        try {
-            Set<String> cities = restaurantService.getAllCities();
-            assertNotNull(cities);
-            assertEquals(3, cities.size());
-            assertTrue(cities.contains("Gothenburg"));
-            assertTrue(cities.contains("Stockholm"));
-            assertTrue(cities.contains("Uppsala"));
-            System.out.println("Success");
-        } catch (Exception e) {
-            e.printStackTrace();
-            fail("Failed with exception " + e.getMessage());
-        }
-    }
+//    @Test
+//    void testDeleteRestaurant() {
+//        long id = 1L;
+//        doNothing().when(restaurantRepository).deleteById(id);
+//
+//        try {
+//            restaurantService.deleteRestaurantById(id);
+//            verify(restaurantRepository, times(1)).deleteById(id);
+//            System.out.println("Deleted succesfully");
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            fail("Deleting failed with exception " + e.getMessage());
+//        }
+//    }
+//
+//    @Test
+//    void testDeleteAllRestaurants(){
+//        Restaurant restaurant1 = new Restaurant();
+//        restaurant1.setId(1L);
+//        restaurant1.setName("Restaurant 1");
+//
+//        Restaurant restaurant2 = new Restaurant();
+//        restaurant2.setId(2L);
+//        restaurant2.setName("Restaurant 2");
+//        System.out.println("Restaurants Ids are : " + restaurant1 + restaurant2);
+//        doNothing().when(restaurantRepository).deleteAll();
+//        try {
+//            restaurantService.deleteAllRestaurants();
+//            verify(restaurantRepository, times(1)).deleteAll();
+//            System.out.println("Deleted succesfully");
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            fail("Deleting failed with exception " + e.getMessage());
+//        }
+//    }
+//    @Test
+//    void testGetAllCities() {
+//        Restaurant restaurant1 = new Restaurant();
+//        restaurant1.setCity("Gothenburg");
+//
+//        Restaurant restaurant2 = new Restaurant();
+//        restaurant2.setCity("Stockholm");
+//
+//        Restaurant restaurant3 = new Restaurant();
+//        restaurant3.setCity("Uppsala");
+//
+//        List<Restaurant> mockRestaurants = List.of(restaurant1, restaurant2, restaurant3);
+//        when(restaurantRepository.findAll()).thenReturn(mockRestaurants);
+//
+//        System.out.println("Cities are: " + mockRestaurants);
+//        try {
+//            Set<String> cities = restaurantService.getAllCities();
+//            assertNotNull(cities);
+//            assertEquals(3, cities.size());
+//            assertTrue(cities.contains("Gothenburg"));
+//            assertTrue(cities.contains("Stockholm"));
+//            assertTrue(cities.contains("Uppsala"));
+//            System.out.println("Success");
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            fail("Failed with exception " + e.getMessage());
+//        }
+//    }
 
   // All restaurants is eventually the same code but restaurants instead of cities
 }
